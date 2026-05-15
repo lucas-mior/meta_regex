66cb256de3f33f994b95459c0e5367f556bb1e12
lucas-mior <lucas.mior@tutamail.com>
2026-05-15 11:52:02 -0300

    style.sh

---
+++ style.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+clang-format -i *.c *.h \
+    --style=file:/home/lucas/.config/clangd/clang-format.yaml
+
+for f in *.c *.h; do 
+    sed -E '
+    s/\<memset\>memset64/g;
+    s/\<memcpy\>memcpy64/g;
+    s/\<memmove\>memmove64/g;
+    s/\<memmem\>memmem64/g;
+    s/\<memchr\>memchr64/g;
+    s/\<strlen\>strlen32/g;
+    s/\<strncmp\>strncmp32/g;
+    ' "$f"
+done
+
+git commit -a -m "apply clang-format"
