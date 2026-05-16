04b3afb6bb705a154c5891a349e9671c5cbea41d
lucas-mior <lucas.mior@tutamail.com>
2026-05-15 22:38:34 -0300

    control dispatcher

--- meta_regex_match.c
+++ meta_regex_match.c
@@ -22,6 +22,162 @@ typedef struct LazyDfa {
     LazyDfaState states[META_MAX_LAZY_DFA_STATES];
 } LazyDfa;
 
+#if defined(ALGO_LAZY_DFA) && defined(ALGO_STATIC_DFA)
+#error "Cannot define both ALGO_LAZY_DFA and ALGO_STATIC_DFA"
+#endif
+
+static int32 try_match_btnfa(MetaRegex *regex, uchar *string,
+                             int32 offset, int64 nmatch, regmatch_t *pmatch);
+static int32 try_match_lazy_dfa(MetaRegex *regex, uchar *string,
+                                int32 offset, int64 nmatch, regmatch_t *pmatch);
+static int32 try_match_dfa(MetaRegex *regex, uchar *string,
+                           int32 offset, int64 nmatch, regmatch_t *pmatch);
+
+static int32
+meta_regex_match(MetaRegex *regex, uchar *string, int64 nmatch,
+                 regmatch_t pmatch[]) {
+    int32 result;
+    int32 use_btnfa;
+    int32 use_lazy_dfa;
+
+    if (regex == NULL) {
+        return REG_NOMATCH;
+    }
+
+    if (pmatch != NULL) {
+        for (int64 k = 0; k < nmatch; k += 1) {
+            pmatch[k].rm_so = -1;
+            pmatch[k].rm_eo = -1;
+        }
+    }
+
+    use_btnfa = 0;
+    use_lazy_dfa = 0;
+
+#if defined(ALGO_BTNFA_ALWAYS)
+    use_btnfa = 1;
+#else
+    if (regex->has_backref) {
+        use_btnfa = 1;
+    } else {
+        int32 has_unsupported;
+
+        has_unsupported = 0;
+        for (int32 i = 0; regex->ops[i].type != META_OP_END; i += 1) {
+            if (regex->ops[i].type == META_OP_WORD_BOUNDARY
+                || regex->ops[i].type == META_OP_WORD_START
+                || regex->ops[i].type == META_OP_WORD_END
+                || regex->ops[i].type == META_OP_NON_WORD_BOUNDARY) {
+                has_unsupported = 1;
+                break;
+            }
+        }
+
+        if (has_unsupported) {
+            printf("TODO: Word boundaries are not supported in DFA yet.\n");
+            use_btnfa;
+        }
+
+#if defined(ALGO_LAZY_DFA)
+        use_lazy_dfa = 1;
+#elif defined(ALGO_STATIC_DFA)
+        use_lazy_dfa = 0;
+#else
+        if (regex->dfa == NULL) {
+            use_lazy_dfa = 1;
+        } else {
+            use_lazy_dfa = 0;
+        }
+#endif
+    }
+#endif
+
+    if (use_btnfa) {
+        if (regex->has_start_anchor) {
+            result = try_match_btnfa(regex, string, 0, nmatch, pmatch);
+            if (result == 0) {
+                return 0;
+            }
+            return REG_NOMATCH;
+        }
+
+        for (int32 j = 0;; j += 1) {
+            uchar b;
+            int32 bit_match;
+
+            b = (uchar)string[j];
+            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
+
+            if (bit_match || regex->can_be_null) {
+                result = try_match_btnfa(regex, string, j, nmatch, pmatch);
+                if (result == 0) {
+                    return 0;
+                }
+            }
+
+            if (b == '\0') {
+                break;
+            }
+        }
+    } else if (use_lazy_dfa) {
+        if (regex->has_start_anchor) {
+            result = try_match_lazy_dfa(regex, string, 0, nmatch, pmatch);
+            if (result == 0) {
+                return 0;
+            }
+            return REG_NOMATCH;
+        }
+
+        for (int32 j = 0;; j += 1) {
+            uchar b;
+            int32 bit_match;
+
+            b = (uchar)string[j];
+            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
+
+            if (bit_match || regex->can_be_null) {
+                result = try_match_lazy_dfa(regex, string, j, nmatch, pmatch);
+                if (result == 0) {
+                    return 0;
+                }
+            }
+
+            if (b == '\0') {
+                break;
+            }
+        }
+    } else {
+        if (regex->has_start_anchor) {
+            result = try_match_dfa(regex, string, 0, nmatch, pmatch);
+            if (result == 0) {
+                return 0;
+            }
+            return REG_NOMATCH;
+        }
+
+        for (int32 j = 0;; j += 1) {
+            uchar b;
+            int32 bit_match;
+
+            b = (uchar)string[j];
+            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
+
+            if (bit_match || regex->can_be_null) {
+                result = try_match_dfa(regex, string, j, nmatch, pmatch);
+                if (result == 0) {
+                    return 0;
+                }
+            }
+
+            if (b == '\0') {
+                break;
+            }
+        }
+    }
+
+    return REG_NOMATCH;
+}
+
 static int32 btnfa_match_at_recursive(MetaOp *ops, uchar *orig, uchar *curr,
                                       int64 nmatch, regmatch_t pmatch[]);
 
@@ -907,155 +1063,6 @@ try_match_lazy_dfa(MetaRegex *regex, uchar *string, int32 offset, int64 nmatch,
     }
 
     return -1;
-}
-
-#if defined(ALGO_LAZY_DFA) && defined(ALGO_STATIC_DFA)
-#error "Cannot define both ALGO_LAZY_DFA and ALGO_STATIC_DFA"
-#endif
-
-static int32
-meta_regex_match(MetaRegex *regex, uchar *string, int64 nmatch,
-                 regmatch_t pmatch[]) {
-    int32 result;
-    int32 use_btnfa;
-    int32 use_lazy_dfa;
-
-    if (regex == NULL) {
-        return REG_NOMATCH;
-    }
-
-    if (pmatch != NULL) {
-        for (int64 k = 0; k < nmatch; k += 1) {
-            pmatch[k].rm_so = -1;
-            pmatch[k].rm_eo = -1;
-        }
-    }
-
-    use_btnfa = 0;
-    use_lazy_dfa = 0;
-
-#if defined(ALGO_BTNFA_ALWAYS)
-    use_btnfa = 1;
-#else
-    if (regex->has_backref) {
-        use_btnfa = 1;
-    } else {
-        int32 has_unsupported;
-
-        has_unsupported = 0;
-        for (int32 i = 0; regex->ops[i].type != META_OP_END; i += 1) {
-            if (regex->ops[i].type == META_OP_WORD_BOUNDARY
-                || regex->ops[i].type == META_OP_WORD_START
-                || regex->ops[i].type == META_OP_WORD_END
-                || regex->ops[i].type == META_OP_NON_WORD_BOUNDARY) {
-                has_unsupported = 1;
-                break;
-            }
-        }
-
-        if (has_unsupported) {
-            printf("TODO: Word boundaries are not supported in DFA yet.\n");
-            exit(1);
-        }
-
-#if defined(ALGO_LAZY_DFA)
-        use_lazy_dfa = 1;
-#elif defined(ALGO_STATIC_DFA)
-        use_lazy_dfa = 0;
-#else
-        if (regex->dfa == NULL) {
-            use_lazy_dfa = 1;
-        } else {
-            use_lazy_dfa = 0;
-        }
-#endif
-    }
-#endif
-
-    if (use_btnfa) {
-        if (regex->has_start_anchor) {
-            result = try_match_btnfa(regex, string, 0, nmatch, pmatch);
-            if (result == 0) {
-                return 0;
-            }
-            return REG_NOMATCH;
-        }
-
-        for (int32 j = 0;; j += 1) {
-            uchar b;
-            int32 bit_match;
-
-            b = (uchar)string[j];
-            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
-
-            if (bit_match || regex->can_be_null) {
-                result = try_match_btnfa(regex, string, j, nmatch, pmatch);
-                if (result == 0) {
-                    return 0;
-                }
-            }
-
-            if (b == '\0') {
-                break;
-            }
-        }
-    } else if (use_lazy_dfa) {
-        if (regex->has_start_anchor) {
-            result = try_match_lazy_dfa(regex, string, 0, nmatch, pmatch);
-            if (result == 0) {
-                return 0;
-            }
-            return REG_NOMATCH;
-        }
-
-        for (int32 j = 0;; j += 1) {
-            uchar b;
-            int32 bit_match;
-
-            b = (uchar)string[j];
-            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
-
-            if (bit_match || regex->can_be_null) {
-                result = try_match_lazy_dfa(regex, string, j, nmatch, pmatch);
-                if (result == 0) {
-                    return 0;
-                }
-            }
-
-            if (b == '\0') {
-                break;
-            }
-        }
-    } else {
-        if (regex->has_start_anchor) {
-            result = try_match_dfa(regex, string, 0, nmatch, pmatch);
-            if (result == 0) {
-                return 0;
-            }
-            return REG_NOMATCH;
-        }
-
-        for (int32 j = 0;; j += 1) {
-            uchar b;
-            int32 bit_match;
-
-            b = (uchar)string[j];
-            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
-
-            if (bit_match || regex->can_be_null) {
-                result = try_match_dfa(regex, string, j, nmatch, pmatch);
-                if (result == 0) {
-                    return 0;
-                }
-            }
-
-            if (b == '\0') {
-                break;
-            }
-        }
-    }
-
-    return REG_NOMATCH;
 }
 
 #endif /* META_REGEX_MATCH_C */
