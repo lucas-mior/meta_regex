5c5d11fd3e23948d96a4de3d1613dab8d88c2159
lucas-mior <lucas.mior@tutamail.com>
2026-05-18 02:48:52 -0300

    improve initialization

--- meta_match_static_dfa.c
+++ meta_match_static_dfa.c
@@ -4,22 +4,14 @@
 static int32
 try_match_static_dfa(MetaRegex *regex, uchar *input, int32 input_len,
                      int32 offset, int64 nmatch, regmatch_t pmatch[]) {
-    DfaState *states;
-    DfaState *current_state_ptr;
-    int32 last_accept;
+    DfaState *states = regex->dfa->states;
+    DfaState *current_state_ptr = &states[regex->dfa->start_state];
+    int32 last_accept = -1;
 
     (void)input_len;
 
-    states = regex->dfa->states;
-    current_state_ptr = &states[regex->dfa->start_state];
-    last_accept = -1;
-
     for (int32 i = offset;; i += 1) {
-        uchar b;
-        int32 next_state_idx;
-        int32 *next_table;
-
-        b = (uchar)input[i];
+        uchar b = (uchar)input[i];
 
         if (current_state_ptr->is_accepting) {
             last_accept = i;
@@ -29,8 +21,8 @@ try_match_static_dfa(MetaRegex *regex, uchar *input, int32 input_len,
             break;
         }
 
-        next_table = current_state_ptr->next;
-        next_state_idx = next_table[b];
+        int32 *next_table = current_state_ptr->next;
+        int32 next_state_idx = next_table[b];
 
         if (next_state_idx == 0) {
             break;
