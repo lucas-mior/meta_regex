bfef00dbd19f9c0dcee9e6ac1975d07c277d59cb
lucas-mior <lucas.mior@tutamail.com>
2026-05-18 02:36:04 -0300

    delete extremely slow lazy tdfa

--- meta_match.c
+++ meta_match.c
@@ -8,29 +8,22 @@
 #include "util.c"
 #include "meta_util.c"
 #include "meta_match_lazy_dfa.c"
-#include "meta_match_lazy_tdfa.c"
 #include "meta_match_btnfa.c"
 #include "meta_match_static_dfa.c"
 
 #if !defined(ALGO_LAZY_DFA)
 #define ALGO_LAZY_DFA 0
 #endif
-#if !defined(ALGO_LAZY_TDFA)
-#define ALGO_LAZY_TDFA 1
-#endif
 #if !defined(ALGO_STATIC_DFA)
 #define ALGO_STATIC_DFA 0
 #endif
 
-#if !ALGO_STATIC_DFA && !ALGO_LAZY_DFA && !ALGO_LAZY_TDFA
+#if !ALGO_STATIC_DFA && !ALGO_LAZY_DFA
 #define ALGO_BTNFA_ALWAYS 1
 #endif
 
 #if ALGO_LAZY_DFA && ALGO_STATIC_DFA
 #error "Cannot define both ALGO_LAZY_DFA and ALGO_STATIC_DFA"
-#endif
-#if ALGO_LAZY_TDFA && ALGO_STATIC_DFA
-#error "Cannot define both ALGO_LAZY_TDFA and ALGO_STATIC_DFA"
 #endif
 
 #define ENUM_PREFIX_ MATCH_ALGO_
@@ -39,7 +32,6 @@
 #define ENUM_FIELDS \
     X(BTNFA) \
     X(LAZY_DFA) \
-    X(LAZY_TDFA) \
     X(STATIC_DFA)
 #include "xenums.c"
 
@@ -72,7 +64,7 @@ meta_regex_match(MetaRegex *regex, uchar *input, int32 input_len, int64 nmatch,
 
         has_unsupported = 0;
 
-#if !ALGO_LAZY_DFA && !ALGO_LAZY_TDFA
+#if !ALGO_LAZY_DFA
         for (int32 i = 0; regex->ops[i].type != META_OP_END; i += 1) {
             if (regex->ops[i].type == META_OP_WORD_BOUNDARY
                 || regex->ops[i].type == META_OP_WORD_START
@@ -87,11 +79,7 @@ meta_regex_match(MetaRegex *regex, uchar *input, int32 input_len, int64 nmatch,
         if (has_unsupported || input_len < USE_DFA_THRESHOLD) {
             algorithm = MATCH_ALGO_BTNFA;
         } else if (regex->re_nsub > 0 && nmatch > 1) {
-#if ALGO_LAZY_TDFA
-            algorithm = MATCH_ALGO_LAZY_TDFA;
-#else
             algorithm = MATCH_ALGO_BTNFA;
-#endif
         } else {
 #if ALGO_LAZY_DFA
             algorithm = MATCH_ALGO_LAZY_DFA;
@@ -126,35 +114,6 @@ meta_regex_match(MetaRegex *regex, uchar *input, int32 input_len, int64 nmatch,
             if (bit_match || regex->can_be_null) {
                 result = try_match_btnfa(regex, input, input_len, j, nmatch,
                                          pmatch);
-                if (result == 0) {
-                    return 0;
-                }
-            }
-
-            if (b == '\0') {
-                break;
-            }
-        }
-    } else if (algorithm == MATCH_ALGO_LAZY_TDFA) {
-        if (regex->has_start_anchor) {
-            result = try_match_lazy_tdfa(regex, input, input_len, 0, nmatch,
-                                         pmatch);
-            if (result == 0) {
-                return 0;
-            }
-            return REG_NOMATCH;
-        }
-
-        for (int32 j = 0;; j += 1) {
-            uchar b;
-            int32 bit_match;
-
-            b = (uchar)input[j];
-            bit_match = (regex->fastmap[b >> 3] & (1 << (b & 7)));
-
-            if (bit_match || regex->can_be_null) {
-                result = try_match_lazy_tdfa(regex, input, input_len, j, nmatch,
-                                             pmatch);
                 if (result == 0) {
                     return 0;
                 }
