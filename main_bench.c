55f12fe8f7ff2fc5671ead2f926afba9bc56cfdd
lucas-mior <lucas.mior@tutamail.com>
2026-05-20 20:00:51 -0300

    try to optimize the tnfa as well

--- main_bench.c
+++ main_bench.c
@@ -105,10 +105,10 @@ bench_input_length_class_max(enum BenchInputLengthClass c) {
 #define ENABLE_BTNFA 1
 #endif
 #if !defined(ENABLE_TNFA)
-#define ENABLE_TNFA 0
+#define ENABLE_TNFA 1
 #endif
 #if !defined(ENABLE_TDFA)
-#define ENABLE_TDFA 1
+#define ENABLE_TDFA 0
 #endif
 #if !defined(ENABLE_LAZY_DFA)
 #define ENABLE_LAZY_DFA 0
