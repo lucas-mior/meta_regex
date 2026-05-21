fb5d382a474d220116dd39610dc1dd4d20116db2
lucas-mior <lucas.mior@tutamail.com>
2026-05-20 23:50:07 -0300

    configure pre processor

--- meta_preproc_0_main.c
+++ meta_preproc_0_main.c
@@ -7,9 +7,15 @@
 #include "meta_preproc_2_gen.c"
 
 static int32
-preproc_parse_int32(char *name, char *value, int32 min_value, int32 max_value) {
+preproc_parse_int32(char *name, char *value, int32 min_value,
+                    int32 max_value) {
     char *end = NULL;
-    int64 parsed;
+    long parsed;
+
+    if (value == NULL) {
+        error("Missing value for %s. Expected %s=N.\n", name, name);
+        exit(EXIT_FAILURE);
+    }
 
     errno = 0;
     parsed = strtol(value, &end, 10);
@@ -24,160 +30,58 @@ preproc_parse_int32(char *name, char *value, int32 min_value, int32 max_value) {
 
 static int32
 preproc_parse_bool(char *name, char *value) {
-    if (strcmp(value, "1") == 0 || strcmp(value, "true") == 0
-        || strcmp(value, "yes") == 0 || strcmp(value, "on") == 0) {
+    if (value == NULL) {
+        error("Missing value for %s. Expected %s=true or %s=false.\n", name,
+              name, name);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(value, "true") == 0) {
         return 1;
     }
-    if (strcmp(value, "0") == 0 || strcmp(value, "false") == 0
-        || strcmp(value, "no") == 0 || strcmp(value, "off") == 0) {
+    if (strcmp(value, "false") == 0) {
         return 0;
     }
-    error("Invalid value for %s: %s. Expected 0/1, true/false, yes/no, or "
-          "on/off.\n",
-          name, value);
+
+    error("Invalid value for %s: %s. Expected true or false.\n", name, value);
     exit(EXIT_FAILURE);
-}
-
-static int32
-preproc_option_is(char *arg, char *name) {
-    return strcmp(arg, name) == 0
-           || (arg[0] == '-' && arg[1] == '-' && strcmp(arg + 2, name) == 0);
-}
-
-static bool
-preproc_parse_config_option(char *arg) {
-    char *value = NULL;
-
-    if (preproc_option_is(arg, "help")) {
-        return false;
-    }
-
-    if (preproc_option_is(arg, "no-static-dfa")) {
-        preproc_config.emit_static_dfa = 0;
-        return true;
-    }
-    if (preproc_option_is(arg, "no-tnfa")) {
-        preproc_config.emit_tnfa = 0;
-        return true;
-    }
-    if (preproc_option_is(arg, "no-tdfa")) {
-        preproc_config.emit_tdfa = 0;
-        return true;
-    }
-    if (preproc_option_is(arg, "no-tdfa-transition-index")) {
-        preproc_config.emit_tdfa_transition_index = 0;
-        return true;
-    }
-
-    if (parse_option(&value, arg, "emit_static_dfa")
-        || parse_option(&value, arg, "emit-static-dfa")
-        || parse_option(&value, arg, "enable_static_dfa")
-        || parse_option(&value, arg, "enable-static-dfa")) {
-        preproc_config.emit_static_dfa
-            = preproc_parse_bool("emit_static_dfa", value);
-        return true;
-    }
-    if (parse_option(&value, arg, "emit_tnfa")
-        || parse_option(&value, arg, "emit-tnfa")
-        || parse_option(&value, arg, "enable_tnfa")
-        || parse_option(&value, arg, "enable-tnfa")) {
-        preproc_config.emit_tnfa = preproc_parse_bool("emit_tnfa", value);
-        return true;
-    }
-    if (parse_option(&value, arg, "emit_tdfa")
-        || parse_option(&value, arg, "emit-tdfa")
-        || parse_option(&value, arg, "enable_tdfa")
-        || parse_option(&value, arg, "enable-tdfa")) {
-        preproc_config.emit_tdfa = preproc_parse_bool("emit_tdfa", value);
-        return true;
-    }
-    if (parse_option(&value, arg, "emit_tdfa_transition_index")
-        || parse_option(&value, arg, "emit-tdfa-transition-index")
-        || parse_option(&value, arg, "enable_tdfa_transition_index")
-        || parse_option(&value, arg, "enable-tdfa-transition-index")) {
-        preproc_config.emit_tdfa_transition_index
-            = preproc_parse_bool("emit_tdfa_transition_index", value);
-        return true;
-    }
-
-    if (parse_option(&value, arg, "max_static_dfa_states")
-        || parse_option(&value, arg, "max-static-dfa-states")) {
-        preproc_config.max_static_dfa_states = preproc_parse_int32(
-            "max_static_dfa_states", value, 0, META_MAX_STATIC_DFA_STATES);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tnfa_tags")
-        || parse_option(&value, arg, "max-tnfa-tags")) {
-        preproc_config.max_tnfa_tags = preproc_parse_int32(
-            "max_tnfa_tags", value, 0, PREPROC_MAX_TNFA_TAGS);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tnfa_states")
-        || parse_option(&value, arg, "max-tnfa-states")) {
-        preproc_config.max_tnfa_states = preproc_parse_int32(
-            "max_tnfa_states", value, 0, PREPROC_MAX_TNFA_STATES);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tnfa_transitions")
-        || parse_option(&value, arg, "max-tnfa-transitions")) {
-        preproc_config.max_tnfa_transitions = preproc_parse_int32(
-            "max_tnfa_transitions", value, 0, PREPROC_MAX_TNFA_TRANSITIONS);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tdfa_states")
-        || parse_option(&value, arg, "max-tdfa-states")) {
-        preproc_config.max_tdfa_states = preproc_parse_int32(
-            "max_tdfa_states", value, 0, PREPROC_MAX_TDFA_STATES);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tdfa_transitions")
-        || parse_option(&value, arg, "max-tdfa-transitions")) {
-        preproc_config.max_tdfa_transitions = preproc_parse_int32(
-            "max_tdfa_transitions", value, 0, PREPROC_MAX_TDFA_TRANSITIONS);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tdfa_registers")
-        || parse_option(&value, arg, "max-tdfa-registers")) {
-        preproc_config.max_tdfa_registers = preproc_parse_int32(
-            "max_tdfa_registers", value, 0, PREPROC_MAX_TDFA_REGISTERS);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tdfa_regops")
-        || parse_option(&value, arg, "max-tdfa-regops")) {
-        preproc_config.max_tdfa_regops = preproc_parse_int32(
-            "max_tdfa_regops", value, 0, PREPROC_MAX_TDFA_REGOPS);
-        return true;
-    }
-    if (parse_option(&value, arg, "max_tdfa_transition_index_entries")
-        || parse_option(&value, arg, "max-tdfa-transition-index-entries")) {
-        preproc_config.max_tdfa_transition_index_entries
-            = preproc_parse_int32("max_tdfa_transition_index_entries", value, 0,
-                                  PREPROC_MAX_TDFA_TRANS_INDEX_ENTRIES);
-        return true;
-    }
-
-    return false;
 }
 
 static void
 usage(void) {
-    fprintf(stderr, "Usage: %s [options] <file.c>\n", program);
+    fprintf(stderr, "Usage: %s [option=value ...] <file.c>\n", program);
     fprintf(stderr, "Options:\n");
-    fprintf(stderr, "  --no-static-dfa | --emit_static_dfa=0\n");
-    fprintf(stderr, "  --no-tnfa       | --emit_tnfa=0\n");
-    fprintf(stderr, "  --no-tdfa       | --emit_tdfa=0\n");
-    fprintf(stderr, "  --no-tdfa-transition-index\n");
-    fprintf(stderr, "  --max_static_dfa_states=N\n");
-    fprintf(stderr, "  --max_tnfa_tags=N\n");
-    fprintf(stderr, "  --max_tnfa_states=N\n");
-    fprintf(stderr, "  --max_tnfa_transitions=N\n");
-    fprintf(stderr, "  --max_tdfa_states=N\n");
-    fprintf(stderr, "  --max_tdfa_transitions=N\n");
-    fprintf(stderr, "  --max_tdfa_registers=N\n");
-    fprintf(stderr, "  --max_tdfa_regops=N\n");
-    fprintf(stderr, "  --max_tdfa_transition_index_entries=N\n");
+    fprintf(stderr, "  help=true\n");
+    fprintf(stderr, "  emit_static_dfa=true|false\n");
+    fprintf(stderr, "  emit_tnfa=true|false\n");
+    fprintf(stderr, "  emit_tdfa=true|false\n");
+    fprintf(stderr, "  emit_tdfa_transition_index=true|false\n");
+    fprintf(stderr, "  max_static_dfa_states=N\n");
+    fprintf(stderr, "  max_tnfa_tags=N\n");
+    fprintf(stderr, "  max_tnfa_states=N\n");
+    fprintf(stderr, "  max_tnfa_transitions=N\n");
+    fprintf(stderr, "  max_tdfa_states=N\n");
+    fprintf(stderr, "  max_tdfa_transitions=N\n");
+    fprintf(stderr, "  max_tdfa_registers=N\n");
+    fprintf(stderr, "  max_tdfa_regops=N\n");
+    fprintf(stderr, "  max_tdfa_transition_index_entries=N\n");
     return;
 }
+
+#define APPLY_BOOL_OPTION(name) \
+    do { \
+        if (name != NULL) { \
+            preproc_config.name = preproc_parse_bool(#name, name); \
+        } \
+    } while (0)
+
+#define APPLY_INT_OPTION(name, min_value, max_value) \
+    do { \
+        if (name != NULL) { \
+            preproc_config.name \
+                = preproc_parse_int32(#name, name, min_value, max_value); \
+        } \
+    } while (0)
 
 int32
 main(int32 argc, char **argv) {
@@ -185,6 +89,22 @@ main(int32 argc, char **argv) {
     int64 file_size = 0;
     char *buffer = NULL;
     char *filename = NULL;
+
+    char *help = NULL;
+    char *emit_static_dfa = NULL;
+    char *emit_tnfa = NULL;
+    char *emit_tdfa = NULL;
+    char *emit_tdfa_transition_index = NULL;
+    char *max_static_dfa_states = NULL;
+    char *max_tnfa_tags = NULL;
+    char *max_tnfa_states = NULL;
+    char *max_tnfa_transitions = NULL;
+    char *max_tdfa_states = NULL;
+    char *max_tdfa_transitions = NULL;
+    char *max_tdfa_registers = NULL;
+    char *max_tdfa_regops = NULL;
+    char *max_tdfa_transition_index_entries = NULL;
+
     program = argv[0];
 
     if (argc < 2) {
@@ -192,16 +112,54 @@ main(int32 argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    for (int32 i = 1; i < argc; i += 1) {
-        if (preproc_option_is(argv[i], "help")) {
+    for (int i = 1; i < argc; i += 1) {
+        PARSE_OPTION(argv[i], help)
+        PARSE_OPTION(argv[i], emit_static_dfa)
+        PARSE_OPTION(argv[i], emit_tnfa)
+        PARSE_OPTION(argv[i], emit_tdfa)
+        PARSE_OPTION(argv[i], emit_tdfa_transition_index)
+        PARSE_OPTION(argv[i], max_static_dfa_states)
+        PARSE_OPTION(argv[i], max_tnfa_tags)
+        PARSE_OPTION(argv[i], max_tnfa_states)
+        PARSE_OPTION(argv[i], max_tnfa_transitions)
+        PARSE_OPTION(argv[i], max_tdfa_states)
+        PARSE_OPTION(argv[i], max_tdfa_transitions)
+        PARSE_OPTION(argv[i], max_tdfa_registers)
+        PARSE_OPTION(argv[i], max_tdfa_regops)
+        PARSE_OPTION(argv[i], max_tdfa_transition_index_entries)
+
+        if (strchr(argv[i], '=') != NULL) {
+            error("Unknown preprocessor option: %s. Options must use exact "
+                  "name=value form.\n", argv[i]);
+            usage();
+            exit(EXIT_FAILURE);
+        }
+
+        filename = argv[i];
+    }
+
+    if (help != NULL) {
+        if (preproc_parse_bool("help", help)) {
             usage();
             exit(EXIT_SUCCESS);
         }
-        if (preproc_parse_config_option(argv[i])) {
-            continue;
-        }
-        filename = argv[i];
     }
+
+    APPLY_BOOL_OPTION(emit_static_dfa);
+    APPLY_BOOL_OPTION(emit_tnfa);
+    APPLY_BOOL_OPTION(emit_tdfa);
+    APPLY_BOOL_OPTION(emit_tdfa_transition_index);
+
+    APPLY_INT_OPTION(max_static_dfa_states, 0, META_MAX_STATIC_DFA_STATES);
+    APPLY_INT_OPTION(max_tnfa_tags, 0, PREPROC_MAX_TNFA_TAGS);
+    APPLY_INT_OPTION(max_tnfa_states, 0, PREPROC_MAX_TNFA_STATES);
+    APPLY_INT_OPTION(max_tnfa_transitions, 0, PREPROC_MAX_TNFA_TRANSITIONS);
+    APPLY_INT_OPTION(max_tdfa_states, 0, PREPROC_MAX_TDFA_STATES);
+    APPLY_INT_OPTION(max_tdfa_transitions, 0, PREPROC_MAX_TDFA_TRANSITIONS);
+    APPLY_INT_OPTION(max_tdfa_registers, 0, PREPROC_MAX_TDFA_REGISTERS);
+    APPLY_INT_OPTION(max_tdfa_regops, 0, PREPROC_MAX_TDFA_REGOPS);
+    APPLY_INT_OPTION(max_tdfa_transition_index_entries, 0,
+                     PREPROC_MAX_TDFA_TRANS_INDEX_ENTRIES);
 
     if (filename == NULL) {
         usage();
@@ -242,3 +200,6 @@ main(int32 argc, char **argv) {
 
     exit(EXIT_SUCCESS);
 }
+
+#undef APPLY_BOOL_OPTION
+#undef APPLY_INT_OPTION
