e0700e8e4a48ab14bf664d55c59aa664e067299e
lucas-mior <lucas.mior@tutamail.com>
2026-05-21 16:15:47 -0300

    benchmark static dfa

--- main_bench.c
+++ main_bench.c
@@ -140,13 +140,13 @@ bench_input_length_class_max(enum BenchInputLengthClass c) {
 #define ENABLE_BTNFA 1
 #endif
 #if !defined(ENABLE_TNFA)
-#define ENABLE_TNFA 1
+#define ENABLE_TNFA 0
 #endif
 #if !defined(ENABLE_TDFA)
-#define ENABLE_TDFA 1
+#define ENABLE_TDFA 0
 #endif
 #if !defined(ENABLE_LAZY_DFA)
-#define ENABLE_LAZY_DFA 1
+#define ENABLE_LAZY_DFA 0
 #endif
 #if !defined(ENABLE_STATIC_DFA)
 #define ENABLE_STATIC_DFA 1
