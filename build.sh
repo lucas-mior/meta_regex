049f662ed872f43b692d662654f89dcb15108109
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 17:37:37 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 # shellcheck disable=SC2086
 
-set -e
-alias trace_on='set -x'
-alias trace_off='{ set +x; } 2>/dev/null'
-
 dir="$(readlink -f "$(dirname "$0")")"
+# shellcheck source=/dev/null
+. "$dir/cbase/common.sh"
+
 cd "$dir" || exit
 cbase="cbase"
 
