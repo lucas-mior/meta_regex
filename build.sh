1061698e76f8217d5582d6a8795691180fc7be07
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 16:52:08 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -43,13 +43,15 @@ if echo "$OS" | grep -q "Linux"; then
     fi
 fi
 
-requested_cc=${CC:-}
 case "$target" in
-"debug"|"test"|"fast_feedback")
-    CC="${requested_cc:-tcc}"
+debug|test)
+    CC="${CC:-tcc}"
+    ;;
+fast_feedback)
+    CC="${CC:-clang}"
     ;;
 *)
-    CC="${requested_cc:-cc}"
+    CC="${CC:-cc}"
     ;;
 esac
 
