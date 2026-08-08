cca9e25a54b69c367229d490012a26da6560244a
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 20:21:54 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ CPPFLAGS="$CPPFLAGS -I${dir}/${cbase} -I $dir"
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall"
-# # CFLAGS="$CFLAGS -Werror=all -Werror=extra"
-# # CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
+CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-missing-field-initializers"
 CFLAGS="$CFLAGS -Wno-type-limits"
 CFLAGS="$CFLAGS -Wno-unknown-pragmas"
