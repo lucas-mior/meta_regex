5c5b874f63719be8df38e285d59859b1683b5c16
lucas-mior <lucas.mior@tutamail.com>
2026-08-31 23:17:19 -0300

    style (whitespace)

--- src/meta_preproc_1_parse_source.c
+++ src/meta_preproc_1_parse_source.c
@@ -240,7 +240,8 @@ parse_source_code(char *buffer, int64 source_len) {
             int64 old_capacity;
             old_capacity = list.capacity;
             list.capacity *= 2;
-            list.items = realloc2(list.items, old_capacity, list.capacity,
+            list.items = realloc2(list.items,
+                                  old_capacity, list.capacity,
                                   SIZEOF(*list.items));
         }
         regex = &list.items[list.count];
